27d1d9f477c20132da40cd121521563f2b157c0d
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-29 11:09:17 +0800

    Adding campgrounds

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require("express");
 const app = express();
+const bodyParse = require('body-parser');
+const bodyParser = require("body-parser");
+const campgrounds = [
+  {
+    name: "Salmon Creek",
+    image: "https://www.photosforclass.com/download/px_1687845",
+  },
+  {
+    name: "Granite hill",
+    image: "https://www.photosforclass.com/download/px_699558",
+  },
+  {
+    name: "Mountain Goat's rest",
+    image: "https://www.photosforclass.com/download/px_1061640",
+  },
+];
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.render("landing");
 });
 app.get("/campgrounds", function (req, res) {
-  const campgrounds = [
-    {
-      name: "Salmon Creek",
-      image: "https://www.photosforclass.com/download/px_1687845",
-    },
-    {
-      name: "Granite hill",
-      image: "https://www.photosforclass.com/download/px_699558",
-    },
-    {
-      name: "Mountain Goat's rest",
-      image: "https://www.photosforclass.com/download/px_1061640",
-    },
-  ];
+
   res.render("campgrounds", { campgrounds, campgrounds });
+});
+app.post("/campgrounds", function (req, res) {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {
+    name: name,
+    image: image
+  };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new.ejs");
 });
 
 app.listen(3000, function () {
