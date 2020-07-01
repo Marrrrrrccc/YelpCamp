3504eef696a036355797e1857cc8c61190f5c9fa
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-07-01 19:12:25 +0800

    add show route

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require("express");
 const app = express();
 const bodyParse = require('body-parser');
 const bodyParser = require("body-parser");
-const campgrounds = [
-  {
-    name: "Salmon Creek",
-    image: "https://www.photosforclass.com/download/px_1687845",
-  },
-  {
-    name: "Granite hill",
-    image: "https://www.photosforclass.com/download/px_699558",
-  },
-  {
-    name: "Mountain Goat's rest",
-    image: "https://www.photosforclass.com/download/px_1061640",
-  },
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
 
-];
+const campgroundShecma = mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+
+const Campground = mongoose.model("Campground", campgroundShecma);
+// Campground.create({
+//   name: "Marc hill",
+//   image: "https://www.photosforclass.com/download/px_699558",
+//   description: "This is marc hill"
+// }, (err, campground) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(campground);
+//   }
+// });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,24 +30,51 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.render("landing");
 });
+//index
 app.get("/campgrounds", function (req, res) {
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  });
 
-  res.render("campgrounds", { campgrounds, campgrounds });
 });
+//create
 app.post("/campgrounds", function (req, res) {
   const name = req.body.name;
   const image = req.body.image;
+  const description = req.body.description
   const newCampground = {
     name: name,
-    image: image
+    image: image,
+    description: description,
   };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
-});
+  Campground.create(newCampground, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+
+});//new
 app.get("/campgrounds/new", function (req, res) {
   res.render("new.ejs");
 });
+//show
+app.get("/campgrounds/:id", function (req, res) {
 
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { foundCampground: foundCampground });
+    }
+  });
+
+})
 app.listen(3000, function () {
   console.log("listening");
 });
